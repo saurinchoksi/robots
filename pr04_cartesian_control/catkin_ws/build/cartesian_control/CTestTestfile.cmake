ca312895dcb55e10b7e4249f50810621f56b2fea
chox <schoksi1@gmail.com>
2017-05-23 16:32:49 -0400

    setup for project 4 on cartesian control

---
+++ pr04_cartesian_control/catkin_ws/build/cartesian_control/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chox/code/robots/pr04_cartesian_control/catkin_ws/src/cartesian_control
+# Build directory: /home/chox/code/robots/pr04_cartesian_control/catkin_ws/build/cartesian_control
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ pr04_cartesian_control/catkin_ws/build/lwr_defs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chox/code/robots/pr04_cartesian_control/catkin_ws/src/lwr_defs
+# Build directory: /home/chox/code/robots/pr04_cartesian_control/catkin_ws/build/lwr_defs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
